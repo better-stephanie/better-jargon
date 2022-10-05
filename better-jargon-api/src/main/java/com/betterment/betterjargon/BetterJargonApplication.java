e94c2b4b10ed84ed7608d69c0e6d6bb1afcef851
better-stephanie <stephanie.ortiz@betterment.com>
2022-10-05 16:55:55 -0400

    Add some debugging

--- better-jargon-api/src/main/java/com/betterment/betterjargon/BetterJargonApplication.java
+++ better-jargon-api/src/main/java/com/betterment/betterjargon/BetterJargonApplication.java
@@ -4,6 +4,8 @@ import com.betterment.betterjargon.words.Word;
 import com.betterment.betterjargon.words.WordRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +17,8 @@ import java.util.List;
 
 @SpringBootApplication
 public class BetterJargonApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(BetterJargonApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterJargonApplication.class, args);
@@ -31,9 +35,9 @@ public class BetterJargonApplication {
 			try {
 				List<Word> words = mapper.readValue(inputStream, typeReference);
 				wordRepository.saveAll(words);
-				System.out.println("Persisted " + words.size() + " words!");
+				LOG.info("Persisted " + words.size() + " words!");
 			} catch (IOException e) {
-				System.out.println("Unable to save words: " + e.getMessage());
+				LOG.error("Unable to save words: " + e.getMessage());
 			}
 		};
 	}
